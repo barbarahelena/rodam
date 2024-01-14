ce2c20a39b924ebad2754304c7dbbddde86fe49d
Barbara Verhaar <barbaraverhaar@MacBookBarbara.local>
2024-01-14 21:27:39 +0100

    add permuted model

--- scripts/9_ruralurban.sh
+++ scripts/9_ruralurban.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -c 48
+#SBATCH -c 68
 #SBATCH --mem=32G
-#SBATCH --time=1:00:00
+#SBATCH --time=2:00:00
 #SBATCH -p 'genoa'
 eval "$(conda shell.bash hook)"
 conda activate xgb
@@ -10,6 +10,15 @@ python /projects/0/prjs0784/rodam/scripts/XGBeast.py \
     -path /projects/0/prjs0784/rodam/rural_urban \
     -x class \
     -n 200 \
-    -t 48 \
+    -t 68 \
     -rand_seed 4321 \
     -param /projects/0/prjs0784/rodam/scripts/param_grid_mb.json
+python /projects/0/prjs0784/rodam/scripts/XGBeast.py \
+    -name rural_urban \
+    -path /projects/0/prjs0784/rodam/rural_urban \
+    -x class \
+    -n 200 \
+    -t 68 \
+    -rand_seed 4321 \
+    -param /projects/0/prjs0784/rodam/scripts/param_grid_mb.json \
+    -permute
