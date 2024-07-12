41eef5b9a9421213420228c360ee4efa22b5842f
Barbara Verhaar <barbaraverhaar@MacBookBarbara.local>
2024-07-12 16:52:42 +0200

    move cov definition to cleaning script

--- scripts/2a_table1.R
+++ scripts/2a_table1.R
@@ -3,7 +3,6 @@
 ## Libraries
 library(tableone)
 library(dplyr)
-library(forcats)
 
 ## Output folder
 resultsfolder <- "results/table1"
@@ -11,9 +10,6 @@ dir.create(resultsfolder, showWarnings = FALSE)
 
 ## Load dataset
 df_new <- rio:: import("data/clinicaldata.RDS")
-df_new$AlcoholBin <- case_when(df_new$Alcohol > 0 ~ "Alcohol use",
-                               df_new$Alcohol == 0 ~ "No alcohol")
-df_new$AlcoholBin <- fct_rev(df_new$AlcoholBin)
 
 ## Table 1
 Table1 <- CreateTableOne(data = df_new, 
@@ -22,10 +18,10 @@ Table1 <- CreateTableOne(data = df_new,
                                   "HT", "AntiHT", "DM", "DMMed", "LipidLowering",
                                   "FecalSample_AB", "FecalSample_Prob", "BristolScale",
                                   "TotalCalories", "Carbohydrates", "Proteins",  
-                                  "Fat", "Fibre", "SaltInt", "Alcohol", "AlcoholBin",
+                                  "Fat", "Fibre", "SodiumInt", "Alcohol", "AlcoholBin",
                                   "PhysAct", "Occupation_binary"), 
                          strata = c("Site"), addOverall = TRUE)
-Table1 <- print(Table1, contDigits = 1, missing = TRUE, nonnormal = c("BristolScale"))
+Table1 <- print(Table1, contDigits = 1, missing = TRUE, nonnormal = c("BristolScale", "PhysAct"))
 Table1 <- as.data.frame(Table1)
 table1 <- Table1 %>% mutate(across(everything(.), ~trimws(.x, which = "both")))
 write.csv2(table1, "results/table1/table1.csv")
